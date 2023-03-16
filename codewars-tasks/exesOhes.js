b1c95367348791b97b287ad462faeddf6d0432c1
Omurbekova Tolkun <tolkun3922@gmail.com>
2023-03-16 15:43:16 +0600

    -added solution of exes and ohes task

--- codewars-tasks/exesOhes.js
+++ codewars-tasks/exesOhes.js
@@ -1,12 +1,24 @@
 /*Check to see if a string has the same amount of 'x's and 'o's. 
 The method must return a boolean and be case insensitive. 
 The string can contain any char.*/
+
 function XO(str) {
-  //code here
+  let xCount = 0;
+  let oCount = 0;
+  let strArr = str.toLowerCase().split("");
+  for (let key in strArr) {
+    if (strArr[key] == "o") {
+      oCount++;
+    }
+    if (strArr[key] == "x") {
+      xCount++;
+    }
+  }
+  return oCount === xCount;
 }
 
-XO("ooxx"); //=> true
-XO("xooxx"); //=> false
-XO("ooxXm"); //=> true
-XO("zpzpzpp"); //=> true // when no 'x' and 'o' is present should return true
-XO("zzoo"); //=> false
+console.log(XO("ooxx")); //=> true
+console.log(XO("xooxx")); //=> false
+console.log(XO("ooxXm")); //=> true
+console.log(XO("zpzpzpp")); //=> true // when no 'x' and 'o' is present should return true
+console.log(XO("zzoo")); //=> false
