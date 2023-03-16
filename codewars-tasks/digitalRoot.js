48debcc8bbcb3a41cb1f1b482520c1d40e12c7c0
Omurbekova Tolkun <tolkun3922@gmail.com>
2023-03-16 14:31:43 +0600

    -solved digitalRoot task

---
+++ codewars-tasks/digitalRoot.js
@@ -0,0 +1,12 @@
+function digital_root(n) {
+  if (n < 10) return n;
+  return digital_root(
+    n
+      .toString()
+      .split("")
+      .reduce((acc, d) => {
+        return acc + +d;
+      }, 0)
+  );
+}
+console.log(digital_root(16));
