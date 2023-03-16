2a45362eb77c591d524236c4c010aa076638abe2
Omurbekova Tolkun <tolkun3922@gmail.com>
2023-03-16 15:00:54 +0600

    -added solve of highest and lowest task

--- codewars-tasks/highAndLow.js
+++ codewars-tasks/highAndLow.js
@@ -1,4 +1,7 @@
-function highAndLow(numbers) {}
-highAndLow("1 2 3 4 5"); // return "5 1"
-highAndLow("1 2 -3 4 5"); // return "5 -3"
-highAndLow("1 9 3 4 -5"); // return "9 -5"
+function highAndLow(numbers) {
+  numbers = numbers.split(" ");
+  return Math.max(...numbers) + " " + Math.min(...numbers);
+}
+console.log(highAndLow("1 2 3 4 5")); // return "5 1"
+console.log(highAndLow("1 2 -3 4 5")); // return "5 -3"
+console.log(highAndLow("1 9 3 4 -5")); // return "9 -5"
