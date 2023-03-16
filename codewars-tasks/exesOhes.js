1fd335d6c214a47da50b58f9348d3229fb2d3ec1
Omurbekova Tolkun <tolkun3922@gmail.com>
2023-03-16 15:04:45 +0600

    -added desc of exes and Ohes task

---
+++ codewars-tasks/exesOhes.js
@@ -0,0 +1,12 @@
+/*Check to see if a string has the same amount of 'x's and 'o's. 
+The method must return a boolean and be case insensitive. 
+The string can contain any char.*/
+function XO(str) {
+  //code here
+}
+
+XO("ooxx"); //=> true
+XO("xooxx"); //=> false
+XO("ooxXm"); //=> true
+XO("zpzpzpp"); //=> true // when no 'x' and 'o' is present should return true
+XO("zzoo"); //=> false
