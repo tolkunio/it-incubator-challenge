62285bcb41505a9580610cc6cd149e392626e60d
Omurbekova Tolkun <tolkun3922@gmail.com>
2023-03-17 14:57:53 +0600

    -added correct solution of filterList task

--- codewars-tasks/filterList.js
+++ codewars-tasks/filterList.js
@@ -2,11 +2,11 @@ function filter_list(l) {
   let numArr = [];
   // Return a new array with the strings filtered out
   for (let i = 0; i < l.length; i++) {
-    if (!isNaN(l[i])) {
+    if (typeof l[i] == "number") {
       numArr.push(l[i]);
     }
   }
   return numArr;
 }
 
-console.log(filter_list([1, 2, "g", "h", 4])); //[1,2,4]
+console.log(filter_list([1, 2, "aasf", "1", "123", 123])); //[1,2,4]
