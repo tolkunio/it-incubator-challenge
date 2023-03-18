0e3aa8c3b5a74c43338cb2019745b36d21224c17
Omurbekova Tolkun <tolkun3922@gmail.com>
2023-03-18 14:40:07 +0600

    -change so;lution

--- freecodecamp-tasks/js/recordCollection.js
+++ freecodecamp-tasks/js/recordCollection.js
@@ -23,14 +23,11 @@ const recordCollection = {
 function updateRecords(records, id, prop, value) {
   if (prop !== "tracks" && value != "") {
     records[id][prop] = value;
-  }
-  if (records[prop] != "tracks" && value != "") {
-    records[id][prop] = value;
   } else if (
-    prop == "tracks" &&
+    prop === "tracks" &&
     records[id].hasOwnProperty("tracks") === false
   ) {
-    records[id][prop] = [value];
+    records[id][prop] = value;
   } else if (prop === "tracks" && value !== "") {
     records[id][prop].push(value);
   } else if (value === "") {
