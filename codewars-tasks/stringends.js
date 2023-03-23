b41dd3ed00df71a47c524f568d36c86129ffdc09
Omurbekova Tolkun <tolkun3922@gmail.com>
2023-03-23 21:33:09 +0600

    -changed stringends task solution

--- codewars-tasks/stringends.js
+++ codewars-tasks/stringends.js
@@ -3,3 +3,15 @@ function solution(str, ending) {
 }
 solution("abc", "bc"); // returns true
 solution("abc", "d"); // returns false
+
+const LOCAL_FORECAST = {
+  yesterday: { low: 61, high: 75 },
+  today: { low: 64, high: 77 },
+  tomorrow: { low: 68, high: 80 },
+};
+
+const {
+  today: { low: lowToday, high: highToday },
+} = LOCAL_FORECAST;
+
+console.log(lowToday);
